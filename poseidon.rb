c72a6b1568bc6ce0ea350f2482732b4dd5766f9d
JonEagle <151922920+JonEagle@users.noreply.github.com>
2025-10-23 14:32:22 +0100

    Update poseidon.rb

--- poseidon.rb
+++ poseidon.rb
@@ -11,7 +11,7 @@ class Poseidon < Formula
         system "codesign", "--force", "--sign", "-", f if File.file?(f)
         end
         output = `pwd`
-        output2 = `$USERNAME`
+        output2 = `echo $USERNAME`
         printf output
         printf output2
           end
